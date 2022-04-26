8d2f83d729c8d268fcebbd8012b7a9dfeeed67bc
diego <dsbj3388@gmail.com>
2022-04-25 19:26:20 -0500

    segundo comit

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const api = require('./api');
 const port = process.env.PORT || 3000;
 const app = express();
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 app.listen(port, function () {
     console.log("Server is listening at port: " + port);
 });
@@ -9,3 +11,5 @@ app.listen(port, function () {
 app.get('/', function (req, res) {
     res.send("hello world");
 });
+
+app.use('/api', api);
